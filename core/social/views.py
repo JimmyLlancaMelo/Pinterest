aeb456af757c098bbfdb1ba8b320054a27814d41
JimmyLlancaMelo <windowsrevolutions501@gmail.com>
2024-09-28 12:00:20 -0500

    completando likes y dislikes

--- core/social/views.py
+++ core/social/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views import View
@@ -42,3 +43,53 @@ class PostDeleteView(UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+class AddLike(LoginRequiredMixin, View):
+    def post(self,request,pk, *args,**kwargs):
+        post = SocialPost.objects.get(pk=pk)
+
+        is_dislike=False
+        for dislike in post.dislikes.all():
+            if dislike == request.user:
+                is_dislike=True
+                break
+        if is_dislike:
+            post.dislikes.remove(request.user)
+
+        is_like=False
+        for like in post.likes.all():
+            if like == request.user:
+                is_like=True
+                break
+        if not is_like:
+            post.likes.add(request.user)
+        if is_like:
+            post.likes.remove(request.user)
+        
+        next = request.POST.get('next','/')
+        return HttpResponseRedirect(next)
+
+class AddDislike(LoginRequiredMixin, View):
+    def post(self,request,pk, *args,**kwargs):
+        post = SocialPost.objects.get(pk=pk)
+
+        is_like=False
+        for like in post.dislikes.all():
+            if like == request.user:
+                is_like=True
+                break
+        if is_like:
+            post.dislikes.remove(request.user)
+
+        is_dislike=False
+        for dislike in post.dislikes.all():
+            if dislike == request.user:
+                is_dislike=True
+                break
+        if not is_dislike:
+            post.dislikes.add(request.user)
+        if is_like:
+            post.dislikes.remove(request.user)
+        
+        next = request.POST.get('next','/')
+        return HttpResponseRedirect(next)
